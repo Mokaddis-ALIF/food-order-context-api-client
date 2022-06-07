dcfa7ca5818138ecc7e4ec7c43dfbf834f90ad5f
Alif-07 <mokaddis.alif@gmail.com>
2022-06-07 14:29:41 +0600

    active button

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
 const Categories = ({ categories, filterItems }) => {
+	const [clicked, setClicked] = useState(false);
+	console.log(clicked);
+
 	return (
 		<div className="d-flex flex-row justify-content-center">
 			{categories.map((category, index) => {
 				return (
 					<Button
 						type="button"
-						variant="danger"
+						variant={clicked === true ? `outline-danger` : `danger`}
 						className="me-3"
 						key={index}
-						onClick={() => filterItems(category)}
+						onClick={() => {
+							filterItems(category);
+							setClicked(true);
+						}}
 					>
 						{category}
 					</Button>
