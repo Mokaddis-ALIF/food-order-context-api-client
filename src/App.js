652c96a50af2b09a3501bfeaad033d67580eee22
Alif-07 <mokaddis.alif@gmail.com>
2022-06-14 19:15:31 +0600

    spinner

--- src/App.js
+++ src/App.js
@@ -13,54 +13,97 @@ import Footer from './components/Footer/Footer';
 import MyOrder from './components/MyOrder/MyOrder';
 import PrivateRoute from './components/Login/PrivateRoute/PrivateRoute';
 import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 
 function App() {
 	const [products, setProducts] = useState([]);
 	const [displayProducts, setDisplayProducts] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [httpError, setHttpError] = useState('');
+
+	const fetchProducts = async () => {
+		const response = await fetch(
+			`https://sheltered-forest-65434.herokuapp.com/products`
+		);
+
+		if (!response.ok) {
+			throw new Error('Something went wrong!');
+		}
+
+		const data = await response.json();
+		setProducts(data);
+		setLoading(false);
+	};
 
 	useEffect(() => {
-		fetch(`https://sheltered-forest-65434.herokuapp.com/products`)
-			.then((res) => res.json())
-			.then((data) => setProducts(data));
-	}, [setProducts]);
+		fetchProducts().catch((error) => {
+			setHttpError(error.message);
+		});
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 1000);
+	}, []);
+
+	if (httpError) {
+		return (
+			<section className="MealsError">
+				<p>{httpError}</p>
+			</section>
+		);
+	}
 
 	return (
-		<CartProvider>
-			<Router>
-				<Header />
-				<Switch>
-					<Route exact path="/">
-						<Shared
-							setDisplayProducts={setDisplayProducts}
-							products={products}
-							displayProducts={displayProducts}
-						/>
-					</Route>
-					<Route exact path="/login">
-						<Login />
-					</Route>
-					<Route exact path="/register">
-						<Register />
-					</Route>
-					<Route exact path="/cart">
-						<Cart />
-					</Route>
-					<PrivateRoute exact path="/order">
-						<Order />
-					</PrivateRoute>
-					<PrivateRoute exact path="/my-orders">
-						<MyOrder />
-					</PrivateRoute>
-					<Route exact path="/details/:id">
-						<MenuDetails />
-					</Route>
-					<Route exact path="*">
-						<NotFound />
-					</Route>
-				</Switch>
-				<Footer />
-			</Router>
-		</CartProvider>
+		<>
+			<CartProvider>
+				<Router>
+					<Header />
+					<Switch>
+						<Route exact path="/">
+							{loading ? (
+								<>
+									<div className="spinner">
+										{[...Array(5)].map((i) => (
+											<Spinner animation="border" variant="danger" />
+										))}
+									</div>
+								</>
+							) : (
+								<>
+									<Shared
+										setDisplayProducts={setDisplayProducts}
+										products={products}
+										displayProducts={displayProducts}
+									/>
+								</>
+							)}
+						</Route>
+						<Route exact path="/login">
+							<Login />
+						</Route>
+						<Route exact path="/register">
+							<Register />
+						</Route>
+						<Route exact path="/cart">
+							<Cart />
+						</Route>
+						<PrivateRoute exact path="/order">
+							<Order />
+						</PrivateRoute>
+						<PrivateRoute exact path="/my-orders">
+							<MyOrder />
+						</PrivateRoute>
+						<Route exact path="/details/:id">
+							<MenuDetails />
+						</Route>
+						<Route exact path="*">
+							<NotFound />
+						</Route>
+					</Switch>
+					<Footer />
+				</Router>
+			</CartProvider>
+		</>
 	);
 }
 
